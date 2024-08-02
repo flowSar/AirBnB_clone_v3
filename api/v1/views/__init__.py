dbb2ac9b0a0bcb3ee79c9cf8e74017948912abd6
flowsar <brahim.sar151@gmail.com>
2024-08-02 19:34:19 +0100

    fix page not found

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,3 +1,5 @@
 from flask import Blueprint
 
 app_views = Blueprint('api', __name__, url_prefix='/api/v1')
+
+from . import index
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
-from api.v1.views import app_views
+from api.v1.views import *
 from flask import jsonify
+
 
 @app_views.route('/status', methods=['GET'])
 def status():
