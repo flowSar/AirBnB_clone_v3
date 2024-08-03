88a10be8b2b9aaac7b57e13cbf06c5333421aecc
Imane-Bjr <boujirimane1@gmail.com>
2024-08-03 14:23:21 -0700

    check errr ors

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Endpoint (route) will be to return the status of your API"""
 
-import os  # Standard library import
-from flask import Flask, jsonify  # Third-party imports
+import os
+from flask import Flask, jsonify
 from flask_cors import CORS
-from api.v1.views import app_views  # Local imports
+from api.v1.views import app_views
 from models import storage
 
 
