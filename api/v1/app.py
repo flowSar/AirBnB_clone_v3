cbd077e46123c526d76f007716f027f2d2d21bd1
Imane-Bjr <boujirimane1@gmail.com>
2024-08-03 14:18:22 -0700

    Task 3 new update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
 """Endpoint (route) will be to return the status of your API"""
 
-from api.v1.views import app_views
-from flask import Flask, jsonify
+import os  # Standard library import
+from flask import Flask, jsonify  # Third-party imports
 from flask_cors import CORS
+from api.v1.views import app_views  # Local imports
 from models import storage
-import os
+
 
 # Create a Flask app
 app = Flask(__name__)
-
-# Enable CORS : Cross-Origin Resource Sharing
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-# Register the blueprint
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
-# Teardown function
 @app.teardown_appcontext
-def close(exception):
+def close_storage(exception):
     """Close the storage session"""
     storage.close()
 
 
-# Custom error handlers
 @app.errorhandler(404)
 def page_not_found(e):
     """Handle 404 errors"""
@@ -39,11 +35,7 @@ def bad_request(e):
 
 
 if __name__ == "__main__":
-    """ Main Function """
-    # Retrieve environment variables or set default values
-    # Default to '0.0.0.0' if not set
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", 5000))  # Default to 5000 if not set
+    port = os.getenv("HBNB_API_PORT", "5000")
 
-    # Run the Flask app
-    app.run(host=host, port=port, threaded=True, debug=True)
+    app.run(host=host, port=int(port), threaded=True, debug=True)
