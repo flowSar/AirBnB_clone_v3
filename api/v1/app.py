c51f16887afcf425b1d4b519122b66f8766ed4cc
Imane-Bjr <boujirimane1@gmail.com>
2024-08-05 14:24:34 -0700

    test

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """ Flask Application """
-
+from models import storage
+from api.v1.views.index import app_views
 from os import environ
 from flask import Flask, make_response, jsonify
 from flask_cors import CORS
-from models import storage
-from api.v1.views import app_views
-
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
@@ -21,7 +19,7 @@ def close_db(error):
 
 @app.errorhandler(404)
 def not_found(error):
-    """ 404 Error Handler
+    """ 404 Error
     ---
     responses:
       404:
@@ -32,6 +30,10 @@ def not_found(error):
 
 if __name__ == "__main__":
     """ Main Function """
-    host = environ.get('HBNB_API_HOST', '0.0.0.0')
-    port = environ.get('HBNB_API_PORT', '5000')
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
     app.run(host=host, port=port, threaded=True)
