95b304efcf3cbdf61d04c99b09faf1ee4f6d4ba6
flowsar <brahim.sar151@gmail.com>
2024-08-05 21:19:50 +0100

    import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,11 +5,11 @@ from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-from api.v1.views.index import get_status, get_stats
+from api.v1.views import index
 from api.v1.views.states import get_states, states_id
 from api.v1.views.cities import *
 from api.v1.views.amenities import get_amenities, amenity_by_id
-from api.v1.views.users import get_users, get_user_by_id
+from api.v1.views import users
 from api.v1.views.places import get_places_by_city, get_places_by_id
 from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
