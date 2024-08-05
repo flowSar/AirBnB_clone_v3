779fa6d4b4842f053123640de88de67d86d0db5a
flowsar <brahim.sar151@gmail.com>
2024-08-05 20:37:37 +0100

    import each route function

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,7 +6,7 @@ from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import status, stats
 from api.v1.views.states import *
-from api.v1.views.cities import get_cities, get_cities_by_id
+from api.v1.views.cities import *
 from api.v1.views.amenities import get_amenities, amenity_by_id
 from api.v1.views.users import get_users, users_by_id
 from api.v1.views.places import get_places_by_city, get_places_by_id
