deb148c0b92dd097e9f61ee5477a242cd660691d
Imane-Bjr <boujirimane1@gmail.com>
2024-08-04 17:22:11 -0700

    Task 3 fixing

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,36 +1,39 @@
 #!/usr/bin/python3
-"""Endpoint (route) will be to return the status of your API"""
-
-import os
-from flask import Flask, jsonify
-from flask_cors import CORS
-from api.v1.views import app_views
+""" Flask Application """
 from models import storage
-
-
+from api.v1.views.index import app_views
+from os import environ
+from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-CORS(app, resources={r"/*": {"origins": "*"}})
-
 app.register_blueprint(app_views)
-
+CORS(app, origins='0.0.0.0')
 
 
 @app.teardown_appcontext
-def close_storage(exception):
-    """tear down: close storage"""
+def close_db(error):
+    """ Close Storage """
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(e):
-    """page not found"""
-    return jsonify({"error": "Not found"}), 404
+def not_found(error):
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
 
 
 if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = os.getenv("HBNB_API_PORT", "5000")
-
-    app.run(host=host, port=port, threaded=True, debug=True)
-
+    """ Main Function """
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,31 +1,22 @@
 #!/usr/bin/python3
-""" Routes for the API """
-
-from api.v1.views import app_views
+""" Index """
+from flask import jsonify
 from models import storage
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-from models.user import User
-from flask import jsonify, make_response
+from api.v1.views import app_views
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
-def get_status():
-    return jsonify({"status": "OK"}), 200
+def status():
+    """ Returns JSON """
+    return jsonify(status="OK")
 
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def get_stats():
-    counts_obj = {
-        "amenities": storage.count(Amenity),
-        "cities": storage.count(City),
-        "places": storage.count(Place),
-        "reviews": storage.count(Review),
-        "states": storage.count(State),
-        "users": storage.count(User)
-    }
-    return jsonify(counts_obj)
-
+def stats():
+    """ Returns the number of each instance type """
+    return jsonify(amenities=storage.count("Amenity"),
+                   cities=storage.count("City"),
+                   places=storage.count("Place"),
+                   reviews=storage.count("Review"),
+                   states=storage.count("State"),
+                   users=storage.count("User"))
