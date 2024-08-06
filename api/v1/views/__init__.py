e2e8af97e969abbf666a71c136ab782d3e085eac
Imane-Bjr <boujirimane1@gmail.com>
2024-08-06 05:59:20 -0700

    test 4

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,22 +2,10 @@
 """Init file for views Blueprint"""
 
 from flask import Blueprint
-print("Initializing Blueprint")
 app_views = Blueprint('app_views', __name__)
-print("Blueprint initialized")
-
-# Import view functions from various modules to register them with the blueprint
-print("Importing views")
-
 from api.v1.views.index import *
-print("Imported index views")
-
 from api.v1.views.states import *
-print("Imported states views")
-
 from api.v1.views.cities import *
-print("Imported cities views")
-
 from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
