2bb969f7188edccf9ec19cd2f5ec82d357acd2f8
flowsar <brahim.sar151@gmail.com>
2024-08-06 12:15:51 +0100

    storag eimport

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,37 +1,59 @@
 #!/usr/bin/python3
-""" Flask Application """
+"""
+Flask Application
+
+This module sets up the Flask application, registers blueprints,
+handles CORS, and defines error handlers and teardown functions.
+"""
+
 from os import environ
 from flask import Flask, make_response, jsonify
 from flask_cors import CORS
-from models import storage
+# from models import storage
 from api.v1.views import app_views
 
 
+# Initialize the Flask application
 app = Flask(__name__)
+
+# Configure the Flask application
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-app.config['JSON_SORT_KEYS'] = False  # Prevent Flask from sorting keys in JSON responses
+
+# Register the blueprint for API views
 app.register_blueprint(app_views)
+
+# Enable CORS for the application
 CORS(app, origins='0.0.0.0')
 
 
 @app.teardown_appcontext
 def close_db(error):
-    """ Close Storage """
+    """ Close Storage
+    This function is called when the app context is torn down. It ensures
+    that the storage is properly closed.
+    """
     storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
     """ 404 Error Handler
-    ---
-    responses:
-      404:
-        description: a resource was not found
+    This function handles 404 errors by returning a JSON response
+    with an error message.
+
+    Parameters:
+        error: The error that caused the handler to be called.
+
+    Returns:
+        A JSON response with a 404 status code and an error message.
     """
     return make_response(jsonify({'error': "Not found"}), 404)
 
 
 if __name__ == "__main__":
+    """ Main Function
+    This function runs the Flask application with the specified host and port.
+    """
     host = environ.get('HBNB_API_HOST', '0.0.0.0')
     port = environ.get('HBNB_API_PORT', '5000')
     app.run(host=host, port=int(port), threaded=True, debug=True)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,10 +7,10 @@ from models import storage
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views import index
-from api.v1.views.states import get_states, states_id
-from api.v1.views.cities import *
-from api.v1.views.amenities import get_amenities, amenity_by_id
-from api.v1.views import users
-from api.v1.views.places import get_places_by_city, get_places_by_id
-from api.v1.views.places_reviews import *
-from api.v1.views.places_amenities import *
+# from api.v1.views.states import get_states, states_id
+# from api.v1.views.cities import *
+# from api.v1.views.amenities import get_amenities, amenity_by_id
+# from api.v1.views import users
+# from api.v1.views.places import get_places_by_city, get_places_by_id
+# from api.v1.views.places_reviews import *
+# from api.v1.views.places_amenities import *
